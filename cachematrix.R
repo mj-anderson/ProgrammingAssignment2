24e6427d181da513d642c6038d92b5afc09e83e2
Mike Anderson <mike@e-nor.com>
2015-10-25 13:45:27 -0500

    First commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,65 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix and cacheSolve are functions designed to find the value of a matrix
+## and then find the mean of the value, and then find the inverse of the mean
 
-## Write a short comment describing this function
+## makeCacheMatrix finds the mean of your matrix while creating 2 new functinos to setmean and getmean
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(mValue = numeric()) {
+  
+  ##Set the mMean object to NULL, so that we can check to see if it exists in the cacheSolve function
+  mMean <- NULL
+  
+  ##Create new function to pass the new MValue if passed
+  set <- function(newMValue) {
+    mValue <<- newMValue
+    mMean <<- NULL
+  }
+  
+  ##Create the get function
+  get <- function() { 
+    mValue
+  }
+  
+  ##Create the setmean function to return the mean with proper scoping
+  setmean <- function(mean) {
+    mMean <<- mean
+  }
+  
+  ##Create getmean Function
+  getmean <- function() {
+    mMean
+  } 
+  
+  #Create the list with newly created functions
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
 }
 
+##cacheSolve functions pulls the value of the mean from cache, checks to see if it exists, and if it does pulls from cache, otherwise 
+##calculates the mean
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(mValue, ...) {
+  ##Get the value of the mean if it exists
+  mMean <- mValue$getmean()
+  
+  ##Check to see if the value exists, if it does pulls from cache
+  if(!is.null(mMean)) {
+    message("getting cached data")
+    ##Returns inverse value of the mean
+    return(solve(mMean))
+  }
+  
+  ##If the mean wasn't null, then find the mean and invert it
+  
+  ##Get the data from makeCacheMatrix
+  data <- mValue$get()
+  
+  ##Calculate the mean of the data
+  mMean <- mean(data, ...)
+  
+  ##Set the mean
+  mValue$setmean(mMean)
+  
+  ##Returns inverse value of the mean
+  solve(mMean)
 }
